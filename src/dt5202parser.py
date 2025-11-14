ce4b995587f90ffbe72aec055c4cfca9644d71a8
cwibisono <wibisono.catur12@gmail.com>
2025-11-14 14:45:17 +0700

    initial commit

---
+++ src/dt5202parser.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+
+from struct import *
+
+def dt5202file(filename):
+    '''
+    Original CAEN_DT5202 Binary Data Format
+    C. Wibisono
+    11/14 '25
+    Usage:
+    To parse the .dat file coming from the Janus DAQ CAEN-DT5202
+    '''
+
+        
+    with open(filename, mode='rb') as f:
+
+        p = Struct("@I") #Unsigned Integer (4 bytes)
+        q = Struct("@B") #Unsigned char (1 bytes)
+        r = Struct("@H") #Unsigned short (2 bytes)
+        s = Struct("@Q") #Unsigned long long (8 bytes)
+        t = Struct("@f") #float (4 bytes)
+        u = Struct("@d") #double (8 bytes)
+
+        #File Header:
+        data_format, = r.unpack(f.read(2))
+        f.read(3)
+        board_type, = r.unpack(f.read(2))
+        run_num, = r.unpack(f.read(2))
+        acq_mode, = q.unpack(f.read(1))
+        en_channels, = r.unpack(f.read(2))
+        time_unit, = q.unpack(f.read(1))
+        time_conv, = t.unpack(f.read(4))
+        start_acq, = s.unpack(f.read(8))
+
+        print("data_format:",data_format,"board_type:",board_type)
+        print("run_num:",run_num,"acq_mode:",acq_mode)
+        print("en_channel:",en_channels,"time_unit:",time_unit)
+        print("time_conv:",time_conv,"start_acq:",start_acq)
+
+
+        #Data: (each list mode consists of event header and event itself)
+        ev_num = 0
+        
+        #Spectroscopy mode:
+        if acq_mode == 1:
+            while(1):
+                #Event_Header:
+                buff = f.read(2)
+                if buff == b'':
+                    return 1
+                ev_size, = r.unpack(buff)
+
+                buff2 = f.read(1)
+                if buff2 == b'':
+                    return 1
+                board_id, = q.unpack(buff2)
+
+                buff3 = f.read(8)
+                if buff3 == b'':
+                    return 1
+                trig_timestamp, = u.unpack(buff3)
+
+                trig_id, = s.unpack(f.read(8))
+                chan_mask, = s.unpack(f.read(8))
+                
+                dim = (ev_size - 27) / 6
+
+                #Event_Data:
+                for k in range(dim):
+                    chan_id, = q.unpack(f.read(1))
+                    data_type, = q.unpack(f.read(1))
+                    LG_PHA, = r.unpack(f.read(2))
+                    HG_PHA, = r.unpack(f.read(2))
+                
+
+
+
+        #Timing Mode:
+        if acq_mode == 2:
+            while(1):
+                #Event_Header:
+                b_counts = 0
+                buff = f.read(2)
+                if buff == b'':
+                    return 1
+                ev_size, = r.unpack(buff)
+            
+                buff2 = f.read(1)
+                if buff2 == b'':
+                    return 1
+                board_id, = q.unpack(buff2)
+
+                buff3 = f.read(8)
+                if buff3 == b'':
+                    return 1
+                tref, = u.unpack(buff3)
+
+                num_hits, = r.unpack(f.read(2))
+                ev_num = ev_num + 1
+                print("ev_size:",ev_size,"board_id:",board_id,"tref:",tref,"num_hits:",num_hits)
+                
+                #Iterate for each event as many as num_hits:
+                for i in range(num_hits):
+                    chan_id, = q.unpack(f.read(1))
+                    data_type, = q.unpack(f.read(1))
+                    b_counts = b_counts+2
+                    if time_unit == 0:
+                        ToA, = p.unpack(f.read(4))
+                        ToT, = r.unpack(f.read(2))
+                        b_counts = b_counts + 6
+                    if time_unit == 1:
+                        ToA, = t.unpack(f.read(4))
+                        ToT, = t.unpack(f.read(4))
+                        b_counts = b_counts + 8
+                    
+                    print("ev_num:",ev_num,"chan_id:",chan_id,"data_type:",data_type,"ToA:",ToA,"ToT:",ToT) 
+    
+            
+                flag_check = b_counts
+                flag_check_comp = ev_size - 13
+                #print("flag_check:",flag_check,"flag_check_comp:",flag_check_comp)
+                if flag_check != flag_check_comp:
+                    break
+            
+
+        #Spectro and Timing Mode:
+        if acq_mode == 3:
+            while(1):
+                #Event_Header:
+                buff = f.read(2)
+                if buff == b'':
+                    break
+                ev_size, = r.unpack(buff)
+                
+                buff2 = f.read(1)
+                if buff2 == b'':
+                    break
+                board_id, = q.unpack(buff2)
+
+                buff3 = f.read(8)
+                if buff3 == b'':
+                    break
+                trig_timestamp, = u.unpack(buff3)
+
+                trig_id, = s.unpack(f.read(8))
+                chan_mask, = s.unpack(f.read(8))
+
+                #Event_Data: Need to verify this further ....
+                dim = (ev_size - 27) / 16 #hard_coded for the LSB Mode only
+                for i in range(dim):
+                    chan_id, = q.unpack(f.read(1))
+                    data_type, = q.unpack(f.read(1))
+                    LG_PHA, = r.unpack(f.read(2))
+                    HG_PHA, = r.unpack(f.read(2))
+                    if time_unit == 0:
+                        ToA, = p.unpack(f.read(4))
+                        ToT, = r.unpack(f.read(2))
+                    if time_unit == 1:
+                        ToA, = t.unpack(f.read(4))
+                        ToT, = t.unpack(f.read(4))
+
+            #Counting Mode:
+            if acq_mode == 4:
+                while(1):
+                    #Event_Header:
+                    buff = f.read(2)
+                    if buff == b'':
+                        break
+                    ev_size, = r.unpack(buff)
+
+                    buff2 = f.read(1)
+                    if buff2 == b'':
+                        break
+                    board_id, = q.unpack(buff2)
+
+                    buff3 = f.read(8)
+                    if buff3 == b'':
+                        break
+                    trig_timestamp, = u.unpack(buff3)
+
+                    trig_id, = s.unpack(f.read(8))
+                    chan_mask, = s.unpack(f.read(8))
+
+                    dim = (ev_size - 27) / 5
+                    #Event_Data:
+                    for i in range(dim):
+                        chan_id, = q.unpack(f.read(1))
+                        counts, = p.unpack(f.read(4))
+
+
+
+if __name__ == "__main__":
+    import sys
+    filein = sys.argv[1]
+    dt5202file(filein)
